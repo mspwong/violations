43b463f6c6dbeb4b07f6cd5f7fd426663719a4b5
Mary Wong <mspwong@hotmail.com>
2017-02-27 14:02:09 -0800

    Reads in violation categories from CSV file

--- lib/processor.rb
+++ lib/processor.rb
@@ -1,5 +1,20 @@
+require 'csv'
+
 class Processor
+  attr_reader :summary
+
+  def initialize(data_file_path)
+    @data_file_path = data_file_path
+    @summary = { }
+  end
 
   def run()
+    violations = CSV.read(@data_file_path, headers: true)
+    violations.each do |row|
+      violation_category = row['violation_category']
+      if !@summary.has_key?(violation_category)
+        @summary[violation_category] = { }
+      end
+    end
   end
 end
--- spec/lib/processor_spec.rb
+++ spec/lib/processor_spec.rb
@@ -1,11 +1,24 @@
 require 'processor'
 
 describe Processor do
-  describe '.run' do
+  describe '::run' do
+    let(:data_file_path)  { 'spec/fixtures/Violations-2012.csv' }
+    let(:summary)         { subject.summary.keys }
+
+    subject { described_class.new(data_file_path) }
+
     before { subject.run }
 
-    it '????' do
-      expect(true).to eq true
+    it 'determines the violation categories' do
+      expect(summary[0]).to eq 'Garbage and Refuse'
+      expect(summary[1]).to eq 'Unsanitary Conditions'
+      expect(summary[2]).to eq 'Animals and Pests'
+      expect(summary[3]).to eq 'Building Conditions'
+      expect(summary[4]).to eq 'Vegetation'
+      expect(summary[5]).to eq 'Chemical Hazards'
+      expect(summary[6]).to eq 'Biohazards'
+      expect(summary[7]).to eq 'Air Pollutants and Odors'
+      expect(summary[8]).to eq 'Retail Food'
     end
   end
 end

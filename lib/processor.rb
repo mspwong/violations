9ca3c48441bf383e82e19fabba25acbd3cfc945d
Mary Wong <mspwong@hotmail.com>
2017-02-27 14:13:30 -0800

    Tracks count for each violation category

--- lib/processor.rb
+++ lib/processor.rb
@@ -12,8 +12,10 @@ class Processor
     violations = CSV.read(@data_file_path, headers: true)
     violations.each do |row|
       violation_category = row['violation_category']
-      if !@summary.has_key?(violation_category)
-        @summary[violation_category] = { }
+      if @summary.has_key?(violation_category)
+        @summary[violation_category][:count] += 1
+      else
+        @summary[violation_category] = { count: 1 }
       end
     end
   end
--- spec/lib/processor_spec.rb
+++ spec/lib/processor_spec.rb
@@ -1,24 +1,37 @@
 require 'processor'
 
 describe Processor do
-  describe '::run' do
-    let(:data_file_path)  { 'spec/fixtures/Violations-2012.csv' }
-    let(:summary)         { subject.summary.keys }
+  describe '#run' do
+    let(:data_file_path)      { 'spec/fixtures/Violations-2012.csv' }
+    let(:summary_categories)  { subject.summary.keys }
+    let(:summary_data)        { subject.summary.values }
 
     subject { described_class.new(data_file_path) }
 
     before { subject.run }
 
     it 'determines the violation categories' do
-      expect(summary[0]).to eq 'Garbage and Refuse'
-      expect(summary[1]).to eq 'Unsanitary Conditions'
-      expect(summary[2]).to eq 'Animals and Pests'
-      expect(summary[3]).to eq 'Building Conditions'
-      expect(summary[4]).to eq 'Vegetation'
-      expect(summary[5]).to eq 'Chemical Hazards'
-      expect(summary[6]).to eq 'Biohazards'
-      expect(summary[7]).to eq 'Air Pollutants and Odors'
-      expect(summary[8]).to eq 'Retail Food'
+      expect(summary_categories[0]).to eq 'Garbage and Refuse'
+      expect(summary_categories[1]).to eq 'Unsanitary Conditions'
+      expect(summary_categories[2]).to eq 'Animals and Pests'
+      expect(summary_categories[3]).to eq 'Building Conditions'
+      expect(summary_categories[4]).to eq 'Vegetation'
+      expect(summary_categories[5]).to eq 'Chemical Hazards'
+      expect(summary_categories[6]).to eq 'Biohazards'
+      expect(summary_categories[7]).to eq 'Air Pollutants and Odors'
+      expect(summary_categories[8]).to eq 'Retail Food'
+    end
+    
+    it 'tracks the violations count for each category' do
+      expect(summary_data[0][:count]).to eq 126
+      expect(summary_data[1][:count]).to eq 83
+      expect(summary_data[2][:count]).to eq 180
+      expect(summary_data[3][:count]).to eq 62
+      expect(summary_data[4][:count]).to eq 67
+      expect(summary_data[5][:count]).to eq 17
+      expect(summary_data[6][:count]).to eq 7
+      expect(summary_data[7][:count]).to eq 2
+      expect(summary_data[8][:count]).to eq 1
     end
   end
 end
